93d82202bdf546b99381be5aa11f87c54447a043
leocuba88 <leocuba88@hotmail.com>
2021-07-15 23:50:22 +0000

    doing

---
+++ app.js
@@ -0,0 +1,18 @@
+const fromDollarToYen = (dollar)  => {
+    yen = dollar*109.8;
+    return yen;
+}
+
+const fromEuroToDollar = (euro)  => {
+    dollar = euro*1.2;
+    return dollar;
+}
+
+const fromYanToPound = (yen)  => {
+    pound = yen*0.0066;
+    return pound;
+}
+
+console.log(fromDollarToYen(10))
+console.log(fromEuroToDollar(10))
+console.log(fromYanToPound(10))
